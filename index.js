ef8e922ebaa0b0f650369ba002f9847589273b28
Amir Hossain <amirhossain.cse113@gmail.com>
2022-05-06 23:33:39 +0600

    get-single-data-api-added

--- index.js
+++ index.js
@@ -26,7 +26,12 @@ async function run(){
             res.send(inventory);
         });
 
-       
+        app.get('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const product = await inventoryCollection.findOne(query);
+            res.send(product);
+        });
 
     }
     finally{
