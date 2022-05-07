1dc594a74fb63f321567c3b013e008c1427f253e
Amir Hossain <amirhossain.cse113@gmail.com>
2022-05-07 18:27:03 +0600

    delete-api-added

--- index.js
+++ index.js
@@ -33,6 +33,16 @@ async function run(){
             res.send(product);
         });
 
+
+
+        //Delete
+        app.delete('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await inventoryCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally{
 
