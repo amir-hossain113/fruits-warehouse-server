226bdb67d78799b418f2687ee6fb2f6af7f04247
Amir Hossain <amirhossain.cse113@gmail.com>
2022-05-10 21:38:43 +0600

    all-api-done

--- index.js
+++ index.js
@@ -33,19 +33,75 @@ async function run(){
             res.send(product);
         });
 
+        //post : add a new product
         app.post('/inventory', async(req, res) => {
             const product = req.body;
             const result = await inventoryCollection.insertOne(product);
             res.send(result);
         })
 
-        //Delete
+        //update product : reStock
+        app.put('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log(req.body)
+            const updatedQuantity = (req.body);
+            console.log(updatedQuantity)
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: updatedQuantity.updatedQuantity
+                }
+            };
+            const result = await inventoryCollection.updateOne(filter, updatedDoc, options)
+            res.send(result);
+        })
+
+        //delivered product : decrease quantity
+        app.put('/inventory/deliver/:id', async(req, res) => {
+            const id = req.params.id;
+            const {newQuantity} = req.body;
+            console.log(req.body.newQuantity, id);
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    quantity: newQuantity
+                }
+            };
+            const result = await inventoryCollection.updateOne(filter, updatedDoc, options)
+            res.send(result);
+
+        })
+
+
+        app.get('/inventory/myItem/:email', async(req, res) => {
+            const {email} = req.params;
+            if(email){
+                const query = {email};
+                const cursor = inventoryCollection.find(query);
+                const inventory = await cursor.toArray();
+                return res.send(inventory);
+            }
+            // return res.send('email')
+        })
+
+        //Delete a product
         app.delete('/inventory/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await inventoryCollection.deleteOne(query);
             res.send(result);
         })
+
+        //Delete product from my item
+        app.delete('/inventory/myItem/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await inventoryCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
     }
     finally{
